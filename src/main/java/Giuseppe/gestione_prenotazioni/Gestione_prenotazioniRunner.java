ae086e68a29f687b7562226722ae26226c3856ec
Peppe1795 <petrucci238@gmail.com>
2023-07-28 13:18:31 +0200

    tabelle e database creati e funzionanti

--- src/main/java/Giuseppe/gestione_prenotazioni/Gestione_prenotazioniRunner.java
+++ src/main/java/Giuseppe/gestione_prenotazioni/Gestione_prenotazioniRunner.java
@@ -2,13 +2,17 @@ package Giuseppe.gestione_prenotazioni;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
 
 import Giuseppe.gestione_prenotazioni.Config.BeanConfiguration;
 import Giuseppe.gestione_prenotazioni.DAO.EdificioService;
 import Giuseppe.gestione_prenotazioni.DAO.PostazioneService;
 import Giuseppe.gestione_prenotazioni.DAO.PrenotazioneService;
 import Giuseppe.gestione_prenotazioni.DAO.UserService;
+import lombok.extern.slf4j.Slf4j;
 
+@Component
+@Slf4j
 public class Gestione_prenotazioniRunner implements CommandLineRunner {
 	@Autowired
 	private BeanConfiguration beanCon;
@@ -27,6 +31,22 @@ public class Gestione_prenotazioniRunner implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+
+		userSrv.save(beanCon.user1());
+		userSrv.save(beanCon.user2());
+		userSrv.save(beanCon.user3());
+
+		edificioSrv.save(beanCon.edificio1());
+		edificioSrv.save(beanCon.edificio2());
+		edificioSrv.save(beanCon.edificio3());
+
+		postazioneSrv.save(beanCon.postazione1());
+		postazioneSrv.save(beanCon.postazione2());
+		postazioneSrv.save(beanCon.postazione3());
+
+		prenotazioneSrv.save(beanCon.prenotazione1());
+		prenotazioneSrv.save(beanCon.prenotazione2());
+		prenotazioneSrv.save(beanCon.prenotazione3());
 	}
 
 }
